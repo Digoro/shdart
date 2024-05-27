feb7833f1eaf7e34cdac10ad17deb0eccd21888c
Digoro <lsh00124@naver.com>
2024-05-27 18:24:49 +0900

    add process signal on bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // pipes
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
+
+  process.on('SIGINT', () => {
+    app.close().then(() => {
+      process.exit(0);
+    })
+  })
+
+  app.listen(3000).then(() => {
+    if (process.send) {
+      process.send('ready');
+    }
+  })
 }
 bootstrap();
